fdb708b1cd0c803fbf979003bda2e3534282d6c3
Michael Davies <michaelstoby@gmail.com>
2024-02-07 22:10:57 +0000

    Change python package name to dndlprof

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='dnd',
+    name='dndlprof',
     version='0.1.0',
     author='Michael Davies',
     author_email='michaelstoby@gmail.com',
